dcee554ca288fd2482fbf7415034ab7bb2201f2b
Vitor Medina Cruz <vitormcruz@gmail.com>
2017-06-29 10:42:56 -0300

    Finishing adding all sections to the new employee view.

--- src/main/groovy/com/vmc/payroll/external/persistence/inMemory/InMemoryEmployeeRepository.groovy
+++ src/main/groovy/com/vmc/payroll/external/persistence/inMemory/InMemoryEmployeeRepository.groovy
@@ -3,21 +3,30 @@ package com.vmc.payroll.external.persistence.inMemory
 import com.vmc.payroll.Employee
 import com.vmc.payroll.api.EmployeeRepository
 import com.vmc.payroll.external.persistence.inMemory.repository.CommonInMemoryRepository
+import com.vmc.payroll.payment.delivery.api.PaymentDelivery
 import com.vmc.payroll.payment.type.api.GenericPaymentType
 import com.vmc.payroll.payment.type.api.PaymentType
 import org.reflections.Reflections
 
 class InMemoryEmployeeRepository extends CommonInMemoryRepository<Employee> implements EmployeeRepository {
 
-    private static Set<Class> paymentTypes
+    private static Set<Class> paymentTypeClasses
+    private static Set<Class> paymentDeliveryClasses
 
     static {
-        paymentTypes = new Reflections("com.vmc.payroll.payment.type").getSubTypesOf(PaymentType)
-        paymentTypes.remove(GenericPaymentType)
+        paymentTypeClasses = new Reflections("com.vmc.payroll.payment.type").getSubTypesOf(PaymentType)
+        paymentTypeClasses.remove(GenericPaymentType)
+
+        paymentDeliveryClasses = new Reflections("com.vmc.payroll.payment.delivery").getSubTypesOf(PaymentDelivery)
     }
 
     @Override
     Collection<Class<PaymentType>> getAllPaymentTypes() {
-        return paymentTypes
+        return new HashSet<Class<PaymentType>>(paymentTypeClasses)
+    }
+
+    @Override
+    Collection<Class<PaymentDelivery>> getAllPaymentDelivery() {
+        return new HashSet<Class<PaymentDelivery>>(paymentDeliveryClasses)
     }
 }
--- src/main/groovy/com/vmc/payroll/external/presentation/vaadin/view/NewEmployeeView.groovy
+++ src/main/groovy/com/vmc/payroll/external/presentation/vaadin/view/NewEmployeeView.groovy
@@ -1,19 +1,23 @@
 package com.vmc.payroll.external.presentation.vaadin.view
 
-import com.vaadin.ui.Button
-import com.vaadin.ui.ComboBox
-import com.vaadin.ui.FormLayout
-import com.vaadin.ui.Notification
-import com.vaadin.ui.RadioButtonGroup
-import com.vaadin.ui.TextField
-import com.vaadin.ui.VerticalLayout
+import com.vaadin.data.HasValue
+import com.vaadin.ui.*
 import com.vmc.payroll.api.EmployeeRepository
+import com.vmc.payroll.payment.delivery.Paymaster
+import com.vmc.payroll.payment.delivery.api.PaymentDelivery
+import com.vmc.payroll.payment.type.Monthly
 import com.vmc.payroll.payment.type.api.PaymentType
 
 class NewEmployeeView extends VerticalLayout{
+    public static final Class<Monthly> DEFAULT_NEW_EMPLOYEE_PAYMENT_TYPE = Monthly
+    public static final Class<Monthly> DEFAULT_NEW_EMPLOYEE_PAYMENT_DELIVERY = Paymaster
+
     private EmployeeRepository employeeRepository
     private Closure cancelNewEmployee
-    private newForm
+    private FormLayout newForm
+    private unionMembershipOption
+    private Map componentesByPaymentType
+    private Map componentesByPaymentDelivery
 
     NewEmployeeView(EmployeeRepository employeeRepository, Closure cancelNewEmployee) {
         this.employeeRepository = employeeRepository
@@ -28,24 +32,97 @@ class NewEmployeeView extends VerticalLayout{
             it.addComponent(new TextField("Name: ").with {it.setRequiredIndicatorVisible(true); it})
             it.addComponent(new TextField("Address: ").with {it.setRequiredIndicatorVisible(true); it})
             it.addComponent(new TextField("Email: ").with{ it.setRequiredIndicatorVisible(true); it})
-            it.addComponent(new ComboBox<PaymentType>("Select a payment type: ").with {
-                it.setItems(employeeRepository.getAllPaymentTypes().collect {it.simpleName})
-                it.setRequiredIndicatorVisible(true)
-                it
-            })
-
-            it.addComponent(new RadioButtonGroup<String>("Is a Union Member").with {
-                it.setItems(["Yes", "No"])
-                it.setValue("Yes")
-                it
-            })
-
+            createPaymentTypesSection(it)
+            createPaymentDeliverySection(it)
+            createUnionMemberSection(it)
             it.addComponent(new Button("Save", {Notification.show("new employee", "to be implemented", Notification.Type.HUMANIZED_MESSAGE)} as Button.ClickListener))
             it.addComponent(new Button("Cancel", cancelNewEmployee as Button.ClickListener))
             it
         }
     }
 
+    def createPaymentDeliverySection(form) {
+        def paymentDeliveryComboBox = createPaymentDeliveryComboBox(form)
+        def paymentDeliveryList = employeeRepository.getAllPaymentDelivery()
+        componentesByPaymentDelivery = paymentDeliveryList.collectEntries{ [(it), it.myVaadinComponents()] }
+        componentesByPaymentDelivery.each {
+            it.value.each {component -> component.setVisible(false)}
+            form.addComponents(*it.value)
+        }
+        paymentDeliveryComboBox.setValue(DEFAULT_NEW_EMPLOYEE_PAYMENT_DELIVERY)
 
+    }
 
+    ComboBox<PaymentDelivery> createPaymentDeliveryComboBox(form) {
+        def paymentTypeComboBox = new ComboBox<PaymentDelivery>("Select a payment delivery: ").with {
+            it.setItems(employeeRepository.getAllPaymentDelivery().collect { it })
+            it.addValueChangeListener({ event -> changePaymentDelivery(event) })
+            it.setItemCaptionGenerator({ it.simpleName })
+            it.setRequiredIndicatorVisible(true)
+            it
+        }
+        form.addComponent(paymentTypeComboBox)
+        return paymentTypeComboBox
+    }
+
+    def changePaymentDelivery(HasValue.ValueChangeEvent<PaymentDelivery> event) {
+        componentesByPaymentDelivery.get(event.getOldValue())?.each {it.setVisible(false)}
+        componentesByPaymentDelivery.get(event.getValue())?.each {it.setVisible(true)}
+    }
+
+    def createPaymentTypesSection(form) {
+        def paymentTypeComboBox = createPaymentTypeComboBox(form)
+        def paymentTypes = employeeRepository.getAllPaymentTypes()
+        componentesByPaymentType = paymentTypes.collectEntries{ [(it), it.myVaadinComponents()] }
+        componentesByPaymentType.each {
+            it.value.each {component -> component.setVisible(false)}
+            form.addComponents(*it.value)
+        }
+        paymentTypeComboBox.setValue(DEFAULT_NEW_EMPLOYEE_PAYMENT_TYPE)
+    }
+
+    ComboBox<PaymentType> createPaymentTypeComboBox(form) {
+        def paymentTypeComboBox = new ComboBox<PaymentType>("Select a payment type: ").with {
+            it.setItems(employeeRepository.getAllPaymentTypes().collect { it })
+            it.addValueChangeListener({ event -> changePaymentType(event) })
+            it.setItemCaptionGenerator({ it.simpleName })
+            it.setRequiredIndicatorVisible(true)
+            it
+        }
+        form.addComponent(paymentTypeComboBox)
+        return paymentTypeComboBox
+    }
+
+    def changePaymentType(HasValue.ValueChangeEvent<PaymentType> event) {
+        componentesByPaymentType.get(event.getOldValue())?.each {it.setVisible(false)}
+        componentesByPaymentType.get(event.getValue())?.each {it.setVisible(true)}
+    }
+
+    def createUnionMemberSection(FormLayout form) {
+        createIsUnionMemberOption(form)
+        createUnionMembershipSection(form)
+    }
+
+    def RadioButtonGroup<String> createIsUnionMemberOption(form) {
+        form.addComponent(new RadioButtonGroup<String>("Is a Union Member").with {
+            it.setItems(["Yes", "No"])
+            it.setValue("No")
+            it.addValueChangeListener({ event -> isUnionMember(event) })
+            it
+        })
+    }
+
+    def createUnionMembershipSection(form) {
+        unionMembershipOption = new TextField("Union Membership Rate: ").with { it.setRequiredIndicatorVisible(true); it }
+        unionMembershipOption.setVisible(false)
+        form.addComponent(unionMembershipOption)
+    }
+
+    def isUnionMember(HasValue.ValueChangeEvent<String> event) {
+        if(event.getValue() == "Yes" && event.getOldValue() == "No"){
+            unionMembershipOption.setVisible(true)
+        }else if(event.getValue() == "No" && event.getOldValue() == "Yes"){
+            unionMembershipOption.setVisible(false)
+        }
+    }
 }
