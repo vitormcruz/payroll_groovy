5c41518946d79b9a1f8f792dc3f70629971c837a
Vitor Medina Cruz <vitormcruz@gmail.com>
2017-07-28 11:46:06 -0300

    Removing builder from sales receipt tests

--- src/test/groovy/com/vmc/payroll/payment/paymentAttachment/SalesReceiptUnitTest.groovy
+++ src/test/groovy/com/vmc/payroll/payment/paymentAttachment/SalesReceiptUnitTest.groovy
@@ -1,34 +1,24 @@
 package com.vmc.payroll.payment.paymentAttachment
 
+import com.vmc.validationNotification.testPreparation.ValidationNotificationTestSetup
 import org.joda.time.DateTime
 import org.junit.Test
-import com.vmc.validationNotification.testPreparation.ValidationNotificationTestSetup
-import com.vmc.validationNotification.builder.GenericBuilder
-
-import static junit.framework.TestCase.fail
 
 class SalesReceiptUnitTest extends ValidationNotificationTestSetup {
 
     @Test
     void "Create a sales receipt providing null to required fields"(){
-        def salesReceiptBuilder = new GenericBuilder(SalesReceipt).withDate(null)
-                                                                  .withAmount(null)
-        salesReceiptBuilder.buildAndDo(
-          {fail("Creating a Sales Receipt without required fields should fail.")},
-          {assert validationObserver.errors.containsAll("payroll.salesreceipt.amount.required",
-                                                        "payroll.salesreceipt.date.required")})
+        def salesReceiptBuilder = SalesReceipt.newSalesReceipt(null, null)
+        assert validationObserver.errors.containsAll("payroll.salesreceipt.amount.required",
+                                                     "payroll.salesreceipt.date.required")
     }
 
     @Test
     void "Create a sales receipt providing valid values to required fields"(){
-        def selesReceipt = new GenericBuilder(SalesReceipt)
         def expectedDateTime = new DateTime()
-        selesReceipt.withConstructorArgs(expectedDateTime, 10)
-        selesReceipt.buildAndDo(
-          {assert it.date == expectedDateTime
-           assert it.amount == 10 },
-          {fail("Creating a Sales Receipt with required fields should be successful.")})
-
+        def selesReceipt = SalesReceipt.newSalesReceipt(expectedDateTime, 10)
+        assert selesReceipt.date == expectedDateTime
+        assert selesReceipt.amount == 10
     }
 
 }
