d0c5ef47b63ada462edcbf9f0997aedb8719ef13
Vitor Medina Cruz <vitormcruz@gmail.com>
2017-08-09 18:29:36 -0300

    Removing unused classes.

--- src/main/groovy/com/vmc/validationNotification/builder/api/BuilderStrategy.groovy
+++
@@ -1,6 +0,0 @@
-package com.vmc.validationNotification.builder.api
-
-interface BuilderStrategy {
-    Boolean successful()
-    def doWithBuiltEntity(builtObject, aSuccessClosure, aFailureClosure)
-}
