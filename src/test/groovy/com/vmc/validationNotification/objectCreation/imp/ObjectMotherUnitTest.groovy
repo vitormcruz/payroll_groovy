32f3168cabbe203cb9ec90f12dcf8e12c81afebd
Vitor Medina Cruz <vitormcruz@gmail.com>
2017-08-29 16:31:43 -0300

    Adding documentation and tests to ObjectMother.

--- src/test/groovy/com/vmc/validationNotification/objectCreation/imp/ObjectMotherUnitTest.groovy
+++ src/test/groovy/com/vmc/validationNotification/objectCreation/imp/ObjectMotherUnitTest.groovy
@@ -14,7 +14,51 @@ class ObjectMotherUnitTest extends ValidationNotificationTestSetup{
     }
 
     @Test
-    void "Create new born requesty must execute postscript on success"(){
+    void "Test scripts receives an instance of the class beeing built"(){
+        def result = []
+        getObjectMother(BuildedClass, {result.add(it instanceof BuildedClass)})
+                                         .addBirthScript({result.add(it instanceof BuildedClass)})
+                                         .addBirthScript({result.add(it instanceof BuildedClass)})
+                                         .addBirthScript({result.add(it instanceof BuildedClass)})
+                                         .createNewBorn()
+
+        assert result == [true, true, true, true]
+    }
+
+    @Test
+    void "Test createNewBornWithScript receives an instance of the class beeing built"(){
+        def result = false
+        getObjectMother(BuildedClass, {}).createNewBornWithScript({result = it instanceof BuildedClass})
+
+        assert result
+    }
+
+
+    @Test
+    void "Test createNewBornWithScript"(){
+        def stringObtained = ""
+        getObjectMother(BuildedClass, {stringObtained += "and the post build script"}).addBirthScript({stringObtained += "a" })
+                                         .addBirthScript({stringObtained += "b" })
+                                         .addBirthScript({stringObtained += "c" })
+                                         .createNewBornWithScript({stringObtained = "ignored all birth scripts, executed this one "})
+
+        assert stringObtained == "ignored all birth scripts, executed this one and the post build script" : "createNewBornWithScript method should have have ignored all previous birth " +
+                                                                                                      "scripts configured except the post build."
+    }
+
+
+    @Test
+    void "Create new born must execute the list of birth scripts in order"(){
+        def stringObtained = ""
+        getObjectMother(BuildedClass, {stringObtained += "d"}).addBirthScript({stringObtained += "a" })
+                                                              .addBirthScript({stringObtained += "b" })
+                                                              .addBirthScript({stringObtained += "c" })
+                                                              .createNewBorn()
+        assert stringObtained == "abcd" : "Builder method should have called birth scripts in order"
+    }
+
+    @Test
+    void "Create new born must execute postscript on success"(){
         def inserted = false
         getObjectMother(BuildedClass, {inserted = true}).createNewBorn()
         assert inserted : "Builder method should have called postBirthScript."
@@ -28,8 +72,8 @@ class ObjectMotherUnitTest extends ValidationNotificationTestSetup{
         assert !inserted : "Post script command was called when the build process failed."
     }
 
-    def getObjectMother(Class aClass, Closure insertCommand){
-        return new ObjectMother(aClass).configurePostBirthScript(insertCommand)
+    def getObjectMother(Class aClass, Closure postBirthScript){
+        return new ObjectMother(aClass).configurePostBirthScript(postBirthScript)
     }
 
     static class BuildedClass {
