7ae28010ffbb15ae26d7c1cf62e8076e8e040b3f
Vitor Medina Cruz <vitormcruz@gmail.com>
2017-09-20 11:22:03 -0300

    Adjust OeintDB configuration, still can't use DateTime.

--- src/main/groovy/com/vmc/payroll/external/config/ProductionServiceLocator.groovy
+++ src/main/groovy/com/vmc/payroll/external/config/ProductionServiceLocator.groovy
@@ -97,11 +97,6 @@ class ProductionServiceLocator extends ServiceLocator{
         new OServerAdmin("localhost").connect("root", "root").createDatabase("test", "document", "memory").close()
         database = new OObjectDatabaseTx("memory:localhost/test").open("admin", "admin")
 
-        [Employee, UnionAssociation, BasicUnionAssociation, NoUnionAssociation, PaymentAttachment, UnionCharge,
-         WorkDoneProof, SalesReceipt, ServiceCharge, TimeCard, PaymentDelivery, AccountTransfer, Mail, Paymaster,
-         PaymentType, Commission, Hourly, Monthly]
-         .each { aClass -> this.@database.getEntityManager().registerEntityClass(aClass) }
-
         OObjectSerializerContext serializerContext = new OObjectSerializerContext();
         serializerContext.bind(new OObjectSerializer<DateTime, Long>() {
             @Override
@@ -116,7 +111,11 @@ class ProductionServiceLocator extends ServiceLocator{
         }, database)
 
         OObjectSerializerHelper.bindSerializerContext(null, serializerContext)
-        this.@database.getEntityManager().registerEntityClass(DateTime)
+
+        [Employee, UnionAssociation, BasicUnionAssociation, NoUnionAssociation, PaymentAttachment, UnionCharge,
+         WorkDoneProof, SalesReceipt, ServiceCharge, TimeCard, PaymentDelivery, AccountTransfer, Mail, Paymaster,
+         PaymentType, Commission, Hourly, Monthly]
+         .each { aClass -> this.@database.getEntityManager().registerEntityClass(aClass) }
 
 //        this.database.save(new TimeCard(DateTime.now(), 100))
 //        this.database.save(new TimeCard(hours: 100))
@@ -125,53 +124,4 @@ class ProductionServiceLocator extends ServiceLocator{
             println(it.getHours() + " - " + it.getDate() )
         }
     }
-
-    public static void main(String[] args) {
-        def start = System.currentTimeMillis()
-        def orientDbServer = OServerMain.create()
-        System.setProperty("ORIENTDB_HOME", new File("").getAbsolutePath());
-        orientDbServer.startup(new OServerConfiguration().with { cfg ->
-            location = "memory"
-            network = new OServerNetworkConfiguration(this)
-            users = [new OServerUserConfiguration(name: "root", password: "root", resources: "*")] as OServerUserConfiguration[]
-            cfg
-        })
-        orientDbServer.activate()
-
-        addShutdownHook {
-            orientDbServer.shutdown()
-        }
-
-        new OServerAdmin("localhost").connect("root", "root").createDatabase("test", "document", "memory").close()
-        OObjectDatabaseTx database = new OObjectDatabaseTx("memory:localhost/test").open("admin", "admin")
-        database.getEntityManager().registerEntityClass(DummyText)
-
-        database.save(new DummyText(text: "Hello"))
-        database.save(new DummyText(text: " World"))
-        database.save(new DummyText(text: "!"))
-        print("\n")
-        database.browseClass(DummyText).each {print(it) }
-        def stop = System.currentTimeMillis()
-        println("\n${stop - start}ms")
-
-        System.exit(0)
-    }
-
-    static class DummyText {
-        private String text
-
-        String getText() {
-            return text
-        }
-
-        void setText(String text) {
-            this.text = text
-        }
-
-        @Override
-        public String toString() {
-            return getText()
-        }
-    }
-
 }
